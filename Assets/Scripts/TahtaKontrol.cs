34062f08987417dbc85e9c3156d04c4c073f4786
busra <busrasimsek929@gmail.com>
2019-07-29 18:49:17 +0300

    2. ekran

---
+++ Assets/Scripts/TahtaKontrol.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TahtaKontrol : MonoBehaviour
+{
+    public List<Transform> Boncuklar = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boncuklar = gameObject.GetComponentsInChildren<Transform>().ToList();
+        foreach(Transform item in Boncuklar.ToList())
+        {
+            if(item.tag != "boncuk")
+            {
+                Boncuklar.Remove(item);
+            }
+            else
+            {
+                item.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void BoncukArttir()
+    {
+        foreach (Transform item in Boncuklar.ToList())
+        {
+        if(item.gameObject.activeSelf == false)
+            {
+                item.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    public void BoncukAzalt()
+    {
+        foreach (Transform item in Boncuklar.ToList())
+        {
+            if (item.gameObject.activeSelf == true)
+            {
+                item.gameObject.SetActive(false);
+                break;
+            }
+        }
+    }
+    // Update is called once per frame
+
+}
